4c5e76a2cd773281e7da360739d7b150de5f72ac
Febrilian Kristiawan <febriliankr@gmail.com>
2021-12-15 02:52:46 +0700

    models

--- models/models.go
+++ models/models.go
@@ -1,47 +1,47 @@
 package models
 
 type Seller struct {
-	Id    string `json:"id" db:"id"`
-	Username  string `json:"username" db:"username"`
-	DisplayName string `json:"display_name" db:"display_name"`
-	Email string `json:"email" db:"email"`
+	Id                string `json:"id" db:"id"`
+	Username          string `json:"username" db:"username"`
+	DisplayName       string `json:"display_name" db:"display_name"`
+	Email             string `json:"email" db:"email"`
 	DisplayPictureUrl string `json:"display_picture_url" db:"display_picture_url"`
-	Address string `json:"address" db:"address"`
+	Address           string `json:"address" db:"address"`
 }
 
 type User struct {
-	Id    string `json:"id"`
-	Username  string `json:"username"`
-	Email string `json:"email"`
+	Id                string `json:"id"`
+	Username          string `json:"username"`
+	Email             string `json:"email"`
 	DisplayPictureUrl string `json:"display_picture_url"`
 }
 
 type RegisterUser struct {
-	Username  string `json:"username" db:"username"`
-	Email string `json:"email" db:"email"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
 	Password string `json:"password" db:"password"`
 }
 
 type UsernameAndPassword struct {
-	Username  string `json:"username" db:"username"`
+	Username string `json:"username" db:"username"`
 	Password string `json:"password" db:"password"`
 }
 
 type IdUsernameAndPassword struct {
-	Id    string `json:"id"`
-	Username  string `json:"username" db:"username"`
+	Id       string `json:"id"`
+	Username string `json:"username" db:"username"`
 	Password string `json:"password" db:"password"`
 }
 
 type LoginResponse struct {
-	Status string `json:"status"`
-	UserExists bool `json:"userExists"`
-	Id    int `json:"id"`
-	Username  string `json:"username" db:"username"`
+	Status     string `json:"status"`
+	UserExists bool   `json:"userExists"`
+	Id         int    `json:"id"`
+	Username   string `json:"username" db:"username"`
 }
 
 type LoginErrorResponse struct {
-	Status string `json:"status"`
-	UserExists bool `json:"userExists"`
-	Message string `json:"message"`
+	Status     string `json:"status"`
+	UserExists bool   `json:"userExists"`
+	Message    string `json:"message"`
 }
